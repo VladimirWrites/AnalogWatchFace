cfdbfb5f9eae7d3d2b49676cdd10fd21cfae9a34
Vladimir Jovanović <j0v4n0v1c@gmail.com>
2018-10-22 20:07:48 +0200

    Version bump: 1.0.1

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright 2017 Vladimir Jovanovic
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 object Versions {
     val kotlin = "1.2.71"
 
@@ -34,8 +18,8 @@ object Versions {
     val compile_sdk = 28
     val build_tools = "28.0.3"
 
-    val version_code = 100
-    val version_name = "1.0.0"
+    val version_code = 101
+    val version_name = "1.0.1"
 }
 
 object Deps {
