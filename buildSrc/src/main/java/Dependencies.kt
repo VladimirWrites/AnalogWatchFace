8afaf1bc9b1f23307c589078c4cdc75964684782
Vladimir Jovanović <j0v4n0v1c@gmail.com>
2019-01-23 23:27:00 +0100

    Update dependencies

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,42 +1,42 @@
 object Versions {
-    val kotlin = "1.3.11"
+    const val kotlin = "1.3.20"
 
-    val wearable_support = "2.4.0"
-    val wearable_support_ui = "28.0.0"
-    val wearable = "2.4.0"
+    const val wearable_support = "2.4.0"
+    const val wearable_support_ui = "28.0.0"
+    const val wearable = "2.4.0"
 
-    val junit = "4.12"
-    val mockito_core = "2.23.0"
-    val mockito_kotlin = "2.0.0"
-    val truth = "0.42"
+    const val junit = "4.12"
+    const val mockito_core = "2.23.4"
+    const val mockito_kotlin = "2.1.0"
+    const val truth = "0.42"
 
-    val gradle_android = "3.2.1"
+    const val gradle_android = "3.3.0"
 
-    val jacoco = "0.8.2"
+    const val jacoco = "0.8.2"
 
-    val min_sdk = 24
-    val target_sdk = 28
-    val compile_sdk = 28
-    val build_tools = "28.0.3"
+    const val min_sdk = 24
+    const val target_sdk = 28
+    const val compile_sdk = 28
+    const val build_tools = "28.0.3"
 
-    val version_code_app = 1400
-    val version_code_wear = 1401
-    val version_name = "1.4.0"
+    const val version_code_app = 1410
+    const val version_code_wear = 1411
+    const val version_name = "1.4.1"
 }
 
 object Deps {
-    val kotlin_stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+    const val kotlin_stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
 
-    val wearable_support = "com.google.android.support:wearable:${Versions.wearable_support}"
-    val wearable_support_ui = "com.android.support:wear:${Versions.wearable_support_ui}"
-    val wearable = "com.google.android.wearable:wearable:${Versions.wearable}"
+    const val wearable_support = "com.google.android.support:wearable:${Versions.wearable_support}"
+    const val wearable_support_ui = "com.android.support:wear:${Versions.wearable_support_ui}"
+    const val wearable = "com.google.android.wearable:wearable:${Versions.wearable}"
 
-    val junit = "junit:junit:${Versions.junit}"
-    val mockito_core = "org.mockito:mockito-core:${Versions.mockito_core}"
-    val mockito_kotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:${Versions.mockito_kotlin}"
-    val truth = "com.google.truth:truth:${Versions.truth}"
-    val kotlin_test = "org.jetbrains.kotlin:kotlin-test:${Versions.kotlin}"
+    const val junit = "junit:junit:${Versions.junit}"
+    const val mockito_core = "org.mockito:mockito-core:${Versions.mockito_core}"
+    const val mockito_kotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:${Versions.mockito_kotlin}"
+    const val truth = "com.google.truth:truth:${Versions.truth}"
+    const val kotlin_test = "org.jetbrains.kotlin:kotlin-test:${Versions.kotlin}"
 
-    val android_gradle_plugin = "com.android.tools.build:gradle:${Versions.gradle_android}"
-    val kotlin_gradle_plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+    const val android_gradle_plugin = "com.android.tools.build:gradle:${Versions.gradle_android}"
+    const val kotlin_gradle_plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
 }
