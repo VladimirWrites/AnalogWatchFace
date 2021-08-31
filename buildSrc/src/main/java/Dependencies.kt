96f62a9dd2f7cb2c411804a07c0db1cf37e93915
VladimirWrites <write@vladimirj.dev>
2021-08-31 08:19:41 +0200

    Update version

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -24,9 +24,9 @@ object Versions {
     const val compile_sdk = 30
     const val build_tools = "30.0.3"
 
-    const val version_code_app = 2801
-    const val version_code_wear = 2802    //Major + Minor + BugFix + 1 for app and 2 for wear
-    const val version_name = "2.8.0"
+    const val version_code_app = 2901
+    const val version_code_wear = 2902    //Major + Minor + BugFix + 1 for app and 2 for wear
+    const val version_name = "2.9.0"
 }
 
 object Deps {
