9d3fb3d8c534050b18d76aa5fc7612fedb88c585
VladimirWrites <write@vladimirj.dev>
2021-09-27 18:56:36 +0200

    Reinitialize Ticks object whenever GetTicks use-case is called
    This way color of the ticks will be drawn correctly

--- wear/src/main/java/com/vlad1m1r/watchface/components/ticks/GetTicks.kt
+++ wear/src/main/java/com/vlad1m1r/watchface/components/ticks/GetTicks.kt
@@ -4,28 +4,29 @@ import com.vlad1m1r.watchface.components.ticks.layout.*
 import com.vlad1m1r.watchface.data.DataStorage
 import com.vlad1m1r.watchface.data.TicksLayoutType
 import javax.inject.Inject
+import javax.inject.Provider
 
 class GetTicks @Inject constructor(
     private val dataStorage: DataStorage,
-    private val ticksLayoutOriginal: TicksLayoutOriginal,
-    private val ticksLayout1: TicksLayout1,
-    private val ticksLayout2: TicksLayout2,
-    private val ticksLayout3: TicksLayout3,
+    private val ticksLayoutOriginal: Provider<TicksLayoutOriginal>,
+    private val ticksLayout1: Provider<TicksLayout1>,
+    private val ticksLayout2: Provider<TicksLayout2>,
+    private val ticksLayout3: Provider<TicksLayout3>,
 ) {
 
     operator fun invoke(): TicksLayout {
         return when (dataStorage.getTicksLayoutType()) {
             TicksLayoutType.ORIGINAL -> {
-                ticksLayoutOriginal
+                ticksLayoutOriginal.get()
             }
             TicksLayoutType.TICKS_LAYOUT_1 -> {
-                ticksLayout1
+                ticksLayout1.get()
             }
             TicksLayoutType.TICKS_LAYOUT_2 -> {
-                ticksLayout2
+                ticksLayout2.get()
             }
             TicksLayoutType.TICKS_LAYOUT_3 -> {
-                ticksLayout3
+                ticksLayout3.get()
             }
         }
     }
