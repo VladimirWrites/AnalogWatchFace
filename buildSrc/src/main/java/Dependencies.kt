5feb7720d1a9f7d058e77e69ad841f7f3eb9b5bc
VladimirWrites <write@vladimirj.dev>
2022-02-13 12:25:37 +0100

    Version 2.14.1

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -25,9 +25,9 @@ object Versions {
     const val target_sdk = 32
     const val compile_sdk = 32
 
-    const val version_code_app = 21401
-    const val version_code_wear = 21402    //Major + Minor + BugFix + 1 for app and 2 for wear
-    const val version_name = "2.14.0"
+    const val version_code_app = 21411
+    const val version_code_wear = 21412    //Major + Minor + BugFix + 1 for app and 2 for wear
+    const val version_name = "2.14.1"
 }
 
 object Deps {
