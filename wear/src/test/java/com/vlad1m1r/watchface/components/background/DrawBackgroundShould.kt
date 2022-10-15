b1686fa5cccff716379a8bd64a1403de4af9b12d
VladimirWrites <write@vladimirj.dev>
2022-10-15 17:33:25 +0200

    Fix tests

--- wear/src/test/java/com/vlad1m1r/watchface/components/background/DrawBackgroundShould.kt
+++ wear/src/test/java/com/vlad1m1r/watchface/components/background/DrawBackgroundShould.kt
@@ -2,6 +2,10 @@ package com.vlad1m1r.watchface.components.background
 
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Color
+import androidx.wear.watchface.DrawMode
+import com.vlad1m1r.watchface.data.state.BackgroundState
+import com.vlad1m1r.watchface.model.Point
 import org.junit.Test
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
@@ -19,14 +23,17 @@ class DrawBackgroundShould {
 
     private val canvasMock = mock<Canvas>()
 
+    private val backgroundState = BackgroundState(false, Color.BLACK, Color.WHITE)
+
+    private val center = Point(0f, 0f)
+
     @Test
     fun switchToAmbientMode() {
         val drawBackground = DrawBackground(
             backgroundBitmapProviderMock
         )
 
-        drawBackground.setInAmbientMode(true)
-        drawBackground(canvasMock, drawMode, center)
+        drawBackground(canvasMock, DrawMode.AMBIENT, center, backgroundState)
 
         verify(canvasMock).drawBitmap(backgroundBitmapProviderMock.getAmbientBitmap(), 0f, 0f, null)
     }
@@ -37,8 +44,7 @@ class DrawBackgroundShould {
             backgroundBitmapProviderMock
         )
 
-        drawBackground.setInAmbientMode(false)
-        drawBackground(canvasMock, drawMode, center)
+        drawBackground(canvasMock, DrawMode.INTERACTIVE, center, backgroundState)
 
         verify(canvasMock).drawBitmap(backgroundBitmapProviderMock.getNormalBitmap(), 0f, 0f, null)
     }
