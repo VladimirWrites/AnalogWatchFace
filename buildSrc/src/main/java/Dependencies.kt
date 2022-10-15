8d175b2af9a589cc05ada249b6174193bfcf7afa
VladimirWrites <write@vladimirj.dev>
2022-10-15 11:37:16 +0200

    Update dependencies

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,24 +1,27 @@
 object Versions {
     const val kotlin = "1.7.0"
+    const val coroutines = "1.6.4"
 
-    const val wearable_support = "2.8.1"
-    const val wearable_support_ui = "1.2.0"
     const val wearable = "2.9.0"
+    const val watchface = "1.2.0-alpha03"
+    const val wear = "1.3.0-alpha03"
     const val wear_remote_interactions = "1.0.0"
 
-    const val androidx_core_ktx = "1.7.0"
-    const val androidx_app_compat = "1.5.1"
-    const val material = "1.5.0"
+    const val androidx_core_ktx = "1.9.0"
+    const val androidx_app_compat = "1.6.0-rc01"
+    const val material = "1.7.0-rc01"
     const val constraintLayout = "2.1.4"
+    const val lifecycle_runtime_ktx = "2.5.1"
+    const val recycler = "1.2.1"
 
     const val hilt = "2.44"
 
     const val junit = "4.13.2"
-    const val mockito = "4.4.0"
+    const val mockito = "4.8.0"
     const val mockito_kotlin = "4.0.0"
     const val truth = "1.1.3"
 
-    const val gradle_android = "7.3.0"
+    const val gradle_android = "7.3.1"
 
     const val min_sdk = 26
     const val target_sdk = 33
@@ -31,11 +34,23 @@ object Versions {
 }
 
 object Deps {
-    const val wearable_support = "com.google.android.support:wearable:${Versions.wearable_support}"
-    const val wearable_support_ui = "androidx.wear:wear:${Versions.wearable_support_ui}"
+    const val coroutines_android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutines}"
+    const val coroutines_core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}"
+
+    const val lifecycle_runtime_ktx = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycle_runtime_ktx}"
+    const val recyclerview = "androidx.recyclerview:recyclerview:${Versions.recycler}"
+
     const val wearable = "com.google.android.wearable:wearable:${Versions.wearable}"
 
     const val wear_remote_interactions = "androidx.wear:wear-remote-interactions:${Versions.wear_remote_interactions}"
+    const val wear = "androidx.wear:wear:${Versions.wear}"
+
+    const val watchface = "androidx.wear.watchface:watchface:${Versions.watchface}"
+    const val watchface_client = "androidx.wear.watchface:watchface-client:${Versions.watchface}"
+    const val watchface_complications_rendering = "androidx.wear.watchface:watchface-complications-rendering:${Versions.watchface}"
+    const val watchface_data = "androidx.wear.watchface:watchface-data:${Versions.watchface}"
+    const val watchface_editor = "androidx.wear.watchface:watchface-editor:${Versions.watchface}"
+    const val watchface_style = "androidx.wear.watchface:watchface-style:${Versions.watchface}"
 
     const val hilt_android = "com.google.dagger:hilt-android:${Versions.hilt}"
     const val hilt_compiler = "com.google.dagger:hilt-compiler:${Versions.hilt}"
