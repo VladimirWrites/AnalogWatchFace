3509acad31fcf39027cdb470d1f212b85ce29213
VladimirWrites <write@vladimirj.dev>
2022-10-15 10:32:26 +0200

    Add new complication types to complications

--- wear/src/main/java/com/vlad1m1r/watchface/ComplicationConfig.kt
+++ wear/src/main/java/com/vlad1m1r/watchface/ComplicationConfig.kt
@@ -1,5 +1,6 @@
 package com.vlad1m1r.watchface
 
+import androidx.wear.watchface.complications.data.ComplicationExperimental
 import androidx.wear.watchface.complications.data.ComplicationType
 
 const val LEFT_COMPLICATION_ID = 100
@@ -8,24 +9,38 @@ const val TOP_COMPLICATION_ID = 102
 const val BOTTOM_COMPLICATION_ID = 103
 const val BACKGROUND_COMPLICATION_ID = 200
 
+@OptIn(ComplicationExperimental::class)
 sealed class ComplicationConfig(val id: Int, val supportedTypes: List<ComplicationType>) {
+
     object Left : ComplicationConfig(
         LEFT_COMPLICATION_ID,
         listOf(
+            ComplicationType.SHORT_TEXT,
+            ComplicationType.DISCRETE_RANGED_VALUE,
+            ComplicationType.GOAL_PROGRESS,
+            ComplicationType.WEIGHTED_ELEMENTS,
+            ComplicationType.LIST,
+            ComplicationType.PROTO_LAYOUT,
             ComplicationType.RANGED_VALUE,
             ComplicationType.MONOCHROMATIC_IMAGE,
-            ComplicationType.SHORT_TEXT,
-            ComplicationType.SMALL_IMAGE
+            ComplicationType.SMALL_IMAGE,
+            ComplicationType.PHOTO_IMAGE,
         )
     )
 
     object Right : ComplicationConfig(
         RIGHT_COMPLICATION_ID,
         listOf(
+            ComplicationType.SHORT_TEXT,
+            ComplicationType.DISCRETE_RANGED_VALUE,
+            ComplicationType.GOAL_PROGRESS,
+            ComplicationType.WEIGHTED_ELEMENTS,
+            ComplicationType.LIST,
+            ComplicationType.PROTO_LAYOUT,
             ComplicationType.RANGED_VALUE,
             ComplicationType.MONOCHROMATIC_IMAGE,
-            ComplicationType.SHORT_TEXT,
-            ComplicationType.SMALL_IMAGE
+            ComplicationType.SMALL_IMAGE,
+            ComplicationType.PHOTO_IMAGE,
         )
     )
 
@@ -36,7 +51,13 @@ sealed class ComplicationConfig(val id: Int, val supportedTypes: List<Complicati
             ComplicationType.RANGED_VALUE,
             ComplicationType.MONOCHROMATIC_IMAGE,
             ComplicationType.SHORT_TEXT,
-            ComplicationType.SMALL_IMAGE
+            ComplicationType.SMALL_IMAGE,
+            ComplicationType.LIST,
+            ComplicationType.PROTO_LAYOUT,
+            ComplicationType.RANGED_VALUE,
+            ComplicationType.MONOCHROMATIC_IMAGE,
+            ComplicationType.SMALL_IMAGE,
+            ComplicationType.PHOTO_IMAGE,
         )
     )
 
@@ -47,14 +68,21 @@ sealed class ComplicationConfig(val id: Int, val supportedTypes: List<Complicati
             ComplicationType.RANGED_VALUE,
             ComplicationType.MONOCHROMATIC_IMAGE,
             ComplicationType.SHORT_TEXT,
-            ComplicationType.SMALL_IMAGE
+            ComplicationType.SMALL_IMAGE,
+            ComplicationType.LIST,
+            ComplicationType.PROTO_LAYOUT,
+            ComplicationType.RANGED_VALUE,
+            ComplicationType.MONOCHROMATIC_IMAGE,
+            ComplicationType.SMALL_IMAGE,
+            ComplicationType.PHOTO_IMAGE,
         )
     )
 
     object Background : ComplicationConfig(
         BACKGROUND_COMPLICATION_ID,
         listOf(
-            ComplicationType.PHOTO_IMAGE
+            ComplicationType.PHOTO_IMAGE,
+            ComplicationType.MONOCHROMATIC_IMAGE,
         )
     )
 
