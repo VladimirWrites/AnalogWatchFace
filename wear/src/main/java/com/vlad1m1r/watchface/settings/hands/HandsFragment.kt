8a313da075d14766f27038ac6abac521baed3fc9
VladimirWrites <write@vladimirj.dev>
2022-10-15 18:48:49 +0200

    Fix optoion shouldKeepHandColorInAmbientMode

--- wear/src/main/java/com/vlad1m1r/watchface/settings/hands/HandsFragment.kt
+++ wear/src/main/java/com/vlad1m1r/watchface/settings/hands/HandsFragment.kt
@@ -5,7 +5,6 @@ import android.view.View
 import androidx.annotation.StringRes
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.vlad1m1r.watchface.data.DataStorage
 import com.vlad1m1r.watchface.settings.Navigator
 import com.vlad1m1r.watchface.settings.base.BaseRecyclerFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,15 +16,12 @@ class HandsFragment(@StringRes private val title: Int) : BaseRecyclerFragment() 
     @Inject
     lateinit var navigator: Navigator
 
-    @Inject
-    lateinit var dataStorage: DataStorage
-
     private lateinit var adapter: HandsAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        adapter = HandsAdapter(navigator, dataStorage, title)
+        adapter = HandsAdapter(navigator, getStateHolder(), title)
         wearableRecyclerView.apply {
             layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
             isEdgeItemsCenteringEnabled = true
