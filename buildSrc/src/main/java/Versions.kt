bfd2f0359d212379b4530bdfc2cbbbaafe61a573
VladimirWrites <write@vladimirj.dev>
2022-10-16 12:47:55 +0200

    Improve and clean build and versioning system

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,11 @@
+object Versions {
+
+    const val min_sdk = 26
+    const val target_sdk = 33
+    const val compile_sdk = 33
+
+    const val version_code_app = 30001
+    const val version_code_wear = 30002    //Major + Minor + BugFix + 1 for app and 2 for wear
+    const val version_name = "3.0.0"
+
+}
