0d54c556bffd7bb3e91fde670ff3baf05b6633bf
VladimirWrites <write@vladimirj.dev>
2022-11-06 14:27:38 +0100

    Version 3.0.5

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -4,8 +4,8 @@ object Versions {
     const val target_sdk = 33
     const val compile_sdk = 33
 
-    const val version_code_app = 30041
-    const val version_code_wear = 30042    //Major + Minor + BugFix + 1 for app and 2 for wear
-    const val version_name = "3.0.4"
+    const val version_code_app = 30051
+    const val version_code_wear = 30052    //Major + Minor + BugFix + 1 for app and 2 for wear
+    const val version_name = "3.0.5"
 
 }
